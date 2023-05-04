64fb75ec9b0f366403f6bb00f97cd1433e1b916e
Marcello <marcellom.azevedo@gmail.com>
2023-05-04 20:54:43 -0300

    inicio

---
+++ bandas/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const axios = require ('axios')
+
+const bandas = {
+    codigoBanda:  {
+        nome:
+
+    }
+}
