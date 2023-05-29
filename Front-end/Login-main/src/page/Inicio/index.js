1b323ac75a927302e5347c2060d27471d1522aa9
Iuri Moura <msiuri@outlook.com>
2023-05-29 20:28:18 -0300

    commit zuado

--- Front-end/Login-main/src/page/Inicio/index.js
+++ Front-end/Login-main/src/page/Inicio/index.js
@@ -1,32 +1,33 @@
-import styles from './Inicio.module.css'
-import Banner from "components/Banner";
-import Titulo from "components/Titulo";
-import Card from "components/Card"
-import { useEffect, useState } from 'react';
+import styles from "./Inicio.module.css";
+import Card from "../../components/Card";
+import Banner from "../../components/Banner";
+import Titulo from "../../components/Titulo";
+
+import { useEffect, useState } from "react";
 
 function Inicio() {
-    const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]);
 
-    useEffect(() => {
-        fetch('https://my-json-server.typicode.com/daniellensousa/cineTag/videos')
-            .then(resposta => resposta.json())
-            .then(dados => {
-                setVideos(dados)
-            })
-    }, []);
-    return (
-        <>
-            {/* <Banner imagem="home" /> */}
-            <Titulo>
-                <h1>Shows disponiveis na sua cidade!</h1>
-            </Titulo>
-            <section className={styles.container}>
-                {videos.map((video) => {
-                    return <Card {...video} key={video.id} />
-                })}
-            </section>
-        </>
-    )
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/daniellensousa/cineTag/videos")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setVideos(dados);
+      });
+  }, []);
+  return (
+    <>
+      { <Banner imagem="home" /> }
+      <Titulo>
+        <h1>Shows disponiveis na sua cidade!</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </section>
+    </>
+  );
 }
 
 export default Inicio;
